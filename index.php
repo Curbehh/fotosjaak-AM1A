2c27ef4abbbe84acd668dc34c9e0415dbbf73922
Mitchell <Mitchelltlam@hotmail.com>
2013-11-19 14:40:33 +0100

    Derde regel

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 	
-	echo "Dit is mijn eerste regel";
+	echo "Dit is mijn eerste regel maar deze heb ik verwijderd";
 	echo "Dit is mijn tweede regel";
+	echo "Dit is mijn derde regel";
 	
 ?>
